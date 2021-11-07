4eb0576ae6daa344ecf5bc885abd26b29c1c4b22
unknown <ferchichi.yasmine@yahoo.com>
2021-11-07 22:12:15 +0100

    fixing issues

--- app/src/main/java/tn/esprit/myhealthyfoodapp/controller/RecepiesActivity.java
+++ app/src/main/java/tn/esprit/myhealthyfoodapp/controller/RecepiesActivity.java
@@ -42,10 +42,25 @@ public class RecepiesActivity extends AppCompatActivity {
         deleteBeforLoad();
 
         MyDatabaseHelper db = new MyDatabaseHelper(RecepiesActivity.this);
-        db.addRecipe("Thai Basil Beef", "https://images.themodernproper.com/billowy-turkey/production/posts/2020/Thai-Basil-Beef-11.jpg?w=1200&auto=compress%2Cformat&fit=crop&fp-x=0.5&fp-y=0.5&dm=1606103993&s=d566d278b6f76803ed574ede574ac7c7", 4, 25, 327, 1,"1,2,3","test inst");
-        db.addRecipe("Gingery Ground Beef", "https://images.themodernproper.com/billowy-turkey/production/posts/2020/Ginger-Ground-Beef-9.jpg?w=667&auto=compress%2Cformat&fit=crop&fp-x=0.5&fp-y=0.5&dm=1606105580&s=f28a3dc2e50e46fbd6e686ea9192dfb9", 4, 20, 269, 1,"1","test inst");
+        db.addRecipe("Thai Basil Beef", "https://images.themodernproper.com/billowy-turkey/production/posts/2020/Thai-Basil-Beef-11.jpg?w=1200&auto=compress%2Cformat&fit=crop&fp-x=0.5&fp-y=0.5&dm=1606103993&s=d566d278b6f76803ed574ede574ac7c7", 4, 25, 327, 1,"1,2,3","Cook the veggies. In a wok or large skillet over medium high heat add the oil, shallots, garlic, ginger and bell peppers and fry for 3 minutes. Push the veggies off to the side.\n" +
+                "Add ground beef.  Turn the heat to high and add the ground beef, breaking it up into small bits with a spoon. (I like to spoon the cooked ground beef into my food processor and pulse it barely a few times to break it into really small pieces).\n" +
+                "Make the sauce. In a small bowl stir together the brown sugar, fish sauce, soy sauce, oyster sauce, cornstarch beef broth and water and add to the pan. Cook for 2 minutes.\n" +
+                "Add the basil, and stir-fry until wilted.\n" +
+                "Serve over hot cooked rice.");
+        db.addRecipe("Gingery Ground Beef", "https://images.themodernproper.com/billowy-turkey/production/posts/2020/Ginger-Ground-Beef-9.jpg?w=667&auto=compress%2Cformat&fit=crop&fp-x=0.5&fp-y=0.5&dm=1606105580&s=f28a3dc2e50e46fbd6e686ea9192dfb9", 4, 20, 269, 1,"1","Step 1\n" +
+                "Heat a large skillet over medium-high heat. Add beef and cook, stirring and crumbling into small pieces until browned, 5 to 7 minutes. Drain excess grease.\n" +
+                "\n" +
+                "Step 2\n" +
+                "Add garlic, ginger, and sesame oil, stirring until fragrant, about 2 minutes. Stir in soy sauce, brown sugar, and red pepper. Cook until some of the sauce absorbs into the beef, about 7 minutes. Add 1/2 of the chopped green onions.\n" +
+                "\n" +
+                "Step 3\n" +
+                "Serve beef over hot cooked rice; garnished with sesame seeds and remaining green onions.");
 
-        db.addRecipe("Avocado salad", "https://www.kindpng.com/picc/m/110-1105729_meal-png-download-image-food-top-view-png.png", 6, 30, 300, 3, "2","test inst");
+        db.addRecipe("Avocado salad", "https://www.kindpng.com/picc/m/110-1105729_meal-png-download-image-food-top-view-png.png", 6, 30, 300, 3, "2","In the bottom of a salad bowl, whisk the lime juice, olive oil, garlic, jalapeno, and salt together. Stir in the herbs. (If making ahead of time, cover and refrigerate up to one day.)\n" +
+                "\n" +
+                "When ready to serve the salad, cut the avocados in half, remove the pits, and cut into cubes.\n" +
+                "\n" +
+                "Add the cubed avocado and cucumber to the bowl with the dressing. Gently toss the avocado and cucumber around the bowl so that they are well coated in the dressing. Taste, and then season with additional salt and your favorite hot sauce (optional).");
         db.addRecipe("Broccoli salad", "https://www.fifteenspatulas.com/wp-content/uploads/2014/01/Broccoli-Salad-Fifteen-Spatulas-7-500x427.jpg", 6, 30, 320, 3, "3","test inst");
         db.addRecipe("Chicken salad", "https://www.eatwell101.com/wp-content/uploads/2019/04/Blackened-Chicken-and-Avocado-Salad-recipe-1.jpg", 6, 30, 250, 3, "3,2","test inst");
 
